05c6e6fef2c1486134e72a48508fceb82f97704b
David Nalesnik <david.nalesnik@gmail.com>
2015-11-10 09:27:40 -0600

    Various enhancements
    --Player has 3 lives.
    --Game speeds up with more successful trips to water.
    --Game over feature.  When player is out of lives, game stops and message
    displayed.
    
    Other changes:
    --initialBugX method improved to deal better with potential off-screen overlaps
    --negative scores are not possible
    --gameState object holds game status info
    --changes to comments
    --formatting changes.  Will need to improve handling of else and block comments.

--- js/app.js
+++ js/app.js
@@ -7,15 +7,16 @@ var dimensions = {
     tileOverlappedHeight: 83
 };
 
-/** ENEMY **/
+var gameState = {
+    gameOver: false,
+    lives: 3,
+    livesLeft: 3
+};
 
-// TODO: make Enemy.y create itself automatically from Enemy.row
-
-// For bug-bug collision detection.  What bugs are in this lane?  Note: this will
-// include our bug if lane is passed this.row.  May need to check for this
-// in caller.
-
-// This is used by both Enemy and Player, so add to both prototypes or leave global?
+// For general collision detection.  What bugs are in this lane?  Note: returned
+// object will include our bug if function is passed this.row after creation.
+// May need to check for this in caller.  As this function is used by both
+// Enemy and Player, it is declared globally.
 function laneContents(lane) {
     var contents = [];
     for (var idx = 0; idx < allEnemies.length; idx++) {
@@ -26,7 +27,7 @@ function laneContents(lane) {
     return contents;
 }
 
-
+/** ENEMY **/
 
 // Enemies our player must avoid
 var Enemy = function(row) {
@@ -34,8 +35,7 @@ var Enemy = function(row) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-    // Row may be specified.  This is used so that when game begins, bugs aren't
-    // accidentally stacked in the same row.  Not necessary now.
+    // Row may be specified.
     if (row) {
         this.row = row;
     } else { this.row = this.getEnemyRow(); }
@@ -49,34 +49,40 @@ var Enemy = function(row) {
     allEnemies.push(this);
 };
 
+// How may times did player reach the water?  Used to set the level.
+var winCount = 0;
+
 // Return a random row for an enemy bug.  Possible rows are numbered 1, 2,
 // or 3 (top row of game board is considered 0).
 Enemy.prototype.getEnemyRow = function() {
     return Math.ceil(Math.random() * 3);
 };
 
-// Assign negative number no bigger than enmeyWidth so bug emerges from off-screen.
+// Assign negative number no bigger than enemyWidth so bug emerges from off-screen.
 // Since lanes are randomly assigned, there is a chance that bugs may overlap.
 // To prevent this, we check our lane for an off-screen or partially off-screen
 // enemy, and put our new bug behind it.  Collision detection will then handle any
 // differences of speed.
 Enemy.prototype.initialBugX = function() {
-    if (allEnemies.length == 0) { return -dimensions.enemyWidth; }
-    for (var idx = 0; idx < allEnemies.length; idx++) {
-        if (allEnemies[idx].row == this.row && allEnemies[idx].x < 0) {
-            // at least 2 bug-lengths separation
-            return allEnemies[idx].x - dimensions.enemyWidth * 2;
-        } else { return -dimensions.enemyWidth; }
+    var maybeColliding = laneContents(this.row);
+    var finalX = -dimensions.enemyWidth;
+    if (maybeColliding.length == 0) { return finalX; }
+    var leftmostX = 0;
+    for (var idx = 0; idx < maybeColliding.length; idx++) {
+        if (maybeColliding[idx] != this && maybeColliding[idx].x < leftmostX) {
+            leftmostX = maybeColliding[idx].x;
+        }
     }
+    return finalX + leftmostX - dimensions.enemyWidth;
 };
 
-// Determine enemy's vertical position based on row.
+// Convert enemy's row into a Y-coordinate on our canvas.
 Enemy.prototype.getEnemyYCoord = function() {
-    var offset = 5;
     var tileOverlap = dimensions.tileFullHeight - dimensions.tileOverlappedHeight;
     var yPos = this.row * dimensions.tileOverlappedHeight;
-    // correct for different alignment of bug relative to image height
-    yPos -= tileOverlap + offset;
+    // correct for different alignment of bug relative to image height; 5
+    // is eyeball factor.
+    yPos -= tileOverlap + 5;
     return yPos;
 };
 
@@ -84,8 +90,11 @@ Enemy.prototype.getEnemyYCoord = function() {
 // TODO: introduce factor to speed up game.
 Enemy.prototype.getEnemyRate = function() {
     var basePixelsPerSecond = 60;
+    // speed up with every 5 wins
+    var levelOffset = Math.floor(winCount / 5) * 20;
+    // speeds range from 1x through 4x
     var multiplier = Math.ceil(Math.random() * 4);
-    return basePixelsPerSecond * multiplier;
+    return basePixelsPerSecond * multiplier + levelOffset;
 };
 
 // Would moving a bug into this lane be bug-collision free?
@@ -93,7 +102,7 @@ Enemy.prototype.canMoveHere = function(lane) {
     var contents = laneContents(lane);
     for (var idx = 0; idx < contents.length; idx++) {
         if (contents[idx].x >= this.x &&
-            contents[idx].x - this.x < dimensions.enemyWidth)
+        contents[idx].x - this.x < dimensions.enemyWidth)
         return false;
     }
     return true;
@@ -107,24 +116,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
     this.x += this.speed * dt;
 
     // When a bug goes off screen, remove it and create a fresh bug.
-    // By the logic of the scenario, it is a new bug that emerges from
-    // the left.  The bug that left the screen presumably keeps on going.
-    // Drawback: new bugs are no longer accessible through a variable.
-    // Advantage: we can vary the number of bugs midstream.
-    // Hmmm.  We're just making an object unreferenceable; waste of memory?
-    // Will GC take care of this?
-    // there are six objects kept in memory: first three bugs and three created
     if (this.x > dimensions.boardWidth) {
         allEnemies.splice(allEnemies.indexOf(this), 1);
         new Enemy();
     }
     // Don't allow bugs in the same lane to overlap.  Move to a usable (adjacent)
     // lane.  If this isn't possible, stay in the current lane, exchanging speeds
-    // with bug directly in the way.
+    // with bug directly in the way.  (An exchange is made so there is no general
+    // slowdown.  Also because it looks like a collision!)
     var inMyLane = laneContents(this.row);
     var dodge = false;
     var avoidMe;
@@ -170,6 +172,7 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.col = 2;
     this.row = 5;
+    // flag for successful arrival at water
     this.madeIt = false;
     this.player = 'images/char-boy.png';
 };
@@ -179,8 +182,8 @@ Player.prototype.setPlayerX = function() {
 };
 
 Player.prototype.setPlayerY = function() {
-    var offset = 30;
-    return this.row * dimensions.tileOverlappedHeight - offset;
+    // 30 is eyeball factor
+    return this.row * dimensions.tileOverlappedHeight - 30;
 };
 
 // If player and bug collide, return true, otherwise false.
@@ -188,6 +191,8 @@ Player.prototype.detectCollision = function() {
     // bugs in our row
     var potentials = laneContents(this.row);
     for (var idx = 0; idx < potentials.length; idx++) {
+        // Distance between left edge of enemy and player is less than
+        // enemy's width ==> collision
         if ((Math.max(this.x, potentials[idx].x) - Math.min(this.x, potentials[idx].x)) <= dimensions.enemyWidth) {
             return true;
         }
@@ -196,17 +201,20 @@ Player.prototype.detectCollision = function() {
 };
 
 Player.prototype.update = function() {
-    if (this.row == 1 && this.madeIt) {
+    if (this.madeIt) {
         this.col = 2;
         this.row = 5;
+        winCount += 1;
         console.log('you made it');
-        score.inc(); // NB
-        // TODO: update speed variable based on number of successes
-        // reset
+        score.update(10);
+        // set flag to reset
         this.madeIt = false;
     }
+    // We're hit
     if (this.detectCollision()) {
-        score.dec();
+        if (score > 0) { score.update(-10); }
+        gameState.livesLeft -= 1;
+        if (gameState.livesLeft == 0) { gameState.gameOver = true; }
         this.col = 2;
         this.row = 5;
     }
@@ -219,12 +227,12 @@ Player.prototype.render = function() {
 };
 
 Player.prototype.handleInput = function(request) {
+    // Honor requests but do not allow player to move off board.
     if (request === 'left' && this.col > 0) this.col -= 1;
     if (request === 'right' && this.col < 4) this.col += 1;
     if (request === 'down' && this.row < 5) this.row += 1;
-    // Signal a success if we get to top, but don't draw hero over the water!
-    // TODO: pause hero over water before resetting position
-    if (request === 'up' && this.row >= 1) {
+    // Signal a success if we get to top, but don't draw player over the water.
+    if (request === 'up') {
         if (this.row == 1) {
             this.madeIt = true;
         } else { this.row -= 1; }
@@ -235,14 +243,11 @@ Player.prototype.handleInput = function(request) {
 
 var Score = function() {
     this.score = 0;
-    this.inc = function() {
-        this.score += 10;
+    this.update = function(offset) {
+        this.score += offset;
+        //if (this.score < 0) { gameState.gameOver = true; }
     };
-    this.dec = function() {
-        this.score -= 10;
-    }
     this.render = function() {
-        // set these variables in engine?
         ctx.font = '30px Verdana';
         ctx.textAlign = 'right';
         ctx.textBaseline = 'top';
@@ -257,11 +262,11 @@ var Score = function() {
 // Place the player object in a variable called player
 
 var allEnemies = [];
-// NOTE: We don't assign instances to global variables.  They will not
-// be garbage collected.
-new Enemy(1);
-new Enemy(2);
-new Enemy(3);
+// NOTE: We don't assign Enemy instances to global variables.  We don't need
+// them once they're off-screen, yet they won't be garbage collected.
+new Enemy();
+new Enemy();
+new Enemy();
 
 var player = new Player();
 
@@ -275,7 +280,6 @@ document.addEventListener('keyup', function(e) {
         38: 'up',
         39: 'right',
         40: 'down',
-        80: 'pause' // how to make this work?
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
